cc03b1cfebade1b256086d2b21b839b8d511f354
pupungeggang6 <pupungeggang6@gmail.com>
2026-09-10 01:48:30 +0900

    temp

--- game/src/asset.cpp
+++ game/src/asset.cpp
@@ -11,7 +11,7 @@ void Asset::loadAsset() {
     for (int i = 0x4E00; i <= 0x6400; i++) codepoints[count++] = i;
     for (int i = 0x3040; i <= 0x309F; i++) codepoints[count++] = i;
     for (int i = 0x30A0; i <= 0x30FF; i++) codepoints[count++] = i;
-    neodgm = LoadFontEx("asset/font/neodgm.ttf", 16, codepoints, count);
+    neodgm = LoadFontEx("asset/font/neodgm.ttf", 32, codepoints, count);
     texture.emplace("back", LoadTexture("asset/image/back.png"));
 }
 
